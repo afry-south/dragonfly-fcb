bcf3803e5df98fd26f877b38dbd24b85b6b8d39e
unknown <A501632@ETE0326.af.se>
2014-09-26 14:07:53 +0200

    restructuring of code -> sensors.c

--- fcb/main.c
+++ fcb/main.c
@@ -1,9 +1,9 @@
 /**
 ******************************************************************************
-* @file    PWM_Test/main.c
-* @author  ÅF Dragonfly - Embedded
-* @version V0.0.1
-* @date    2014-09-19
+* @file    fcb/main.c
+* @author  ÅF Dragonfly - Embedded Systems
+* @version v. 0.0.1
+* @date    2014-09-26
 * @brief   Flight Control program for the ÅF Dragonfly quadcopter
 ******************************************************************************
 **/
@@ -11,28 +11,13 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include <stdio.h>
-#include "stm32f3_discovery_lsm303dlhc.h"
-#include "stm32f3_discovery_l3gd20.h"
+#include "sensors.c"
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
-
 /* Private macro -------------------------------------------------------------*/
-#define ABS(x)         (x < 0) ? (-x) : x
-
-#define L3G_Sensitivity_250dps     (float)   114.285f         /*!< gyroscope sensitivity with 250 dps full scale [LSB/dps] */
-#define L3G_Sensitivity_500dps     (float)    57.1429f        /*!< gyroscope sensitivity with 500 dps full scale [LSB/dps] */
-#define L3G_Sensitivity_2000dps    (float)    14.285f	      /*!< gyroscope sensitivity with 2000 dps full scale [LSB/dps] */
-#define PI                         (float)     3.14159265f
-
-#define LSM_Acc_Sensitivity_2g     (float)     1.0f            /*!< accelerometer sensitivity with 2 g full scale [LSB/mg] */
-#define LSM_Acc_Sensitivity_4g     (float)     0.5f            /*!< accelerometer sensitivity with 4 g full scale [LSB/mg] */
-#define LSM_Acc_Sensitivity_8g     (float)     0.25f           /*!< accelerometer sensitivity with 8 g full scale [LSB/mg] */
-#define LSM_Acc_Sensitivity_16g    (float)     0.0834f         /*!< accelerometer sensitivity with 12 g full scale [LSB/mg] */
-
 
 /* Private variables ---------------------------------------------------------*/
-
 TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure2;	// TIM2
 TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure4; // TIM3
 TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure3; // TIM4
@@ -80,6 +65,7 @@ RCC_ClocksTypeDef RCC_Clocks;
 
 uint32_t i = 0; // counter (delete later)
 
+// Sensor readings arrays
 float MagBuffer[3] = {0.0f}, AccBuffer[3] = {0.0f}, GyroBuffer[3] = {0.0f};
 
 /* Private functions -----------------------------------------------*/
@@ -94,12 +80,6 @@ static void PWM_In_Setup(void);
 static void TIM2_Setup(void);
 
 static uint16_t getPWM_CCR(float dutycycle);
-
-static void GyroConfig(void);
-static void GyroReadAngRate(float* pfData);
-static void CompassConfig(void);
-static void CompassReadAcc(float* pfData);
-static void CompassReadMag(float* pfData);
 
 /**
 * @brief  Main program.
@@ -169,32 +149,32 @@ void TIM3_IRQHandler()
 	{
 		TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
 		i++;
-		if(i == 1000) {
+		if(i == 3000) {
 			/* Read Gyro Angular rate data */
-			//GyroReadAngRate(GyroBuffer);
+			GyroReadAngRate(GyroBuffer);
 			/* Read Compass data */
-			//CompassReadMag(MagBuffer);
-			//CompassReadAcc(AccBuffer);
+			CompassReadMag(MagBuffer);
+			CompassReadAcc(AccBuffer);
 
-			// 40% dutycycle for 10 sec
-//			TIM4->CCR1 = getPWM_CCR(0.40);
-//			TIM4->CCR2 = getPWM_CCR(0.40);
-//			TIM4->CCR3 = getPWM_CCR(0.40);
-//			TIM4->CCR4 = getPWM_CCR(0.40);
+			// 40% dutycycle for 30 sec (if interrupt at 100 Hz)
+			TIM4->CCR1 = getPWM_CCR(0.40);
+			TIM4->CCR2 = getPWM_CCR(0.40);
+			TIM4->CCR3 = getPWM_CCR(0.40);
+			TIM4->CCR4 = getPWM_CCR(0.40);
 		}
-		else if(i == 2000) {
+		else if(i == 6000) {
 			/* Read Gyro Angular rate data */
-			//GyroReadAngRate(GyroBuffer);
+			GyroReadAngRate(GyroBuffer);
 			/* Read Compass data */
-			//CompassReadMag(MagBuffer);
-			//CompassReadAcc(AccBuffer);
+			CompassReadMag(MagBuffer);
+			CompassReadAcc(AccBuffer);
 
-			// 50% dutycycle for 10 sec
-//			TIM4->CCR1 = getPWM_CCR(0.50);
-//			TIM4->CCR2 = getPWM_CCR(0.50);
-//			TIM4->CCR3 = getPWM_CCR(0.50);
-//			TIM4->CCR4 = getPWM_CCR(0.50);
-//			i = 0;
+			// 50% dutycycle for 30 sec (if interrupt at 100 Hz)
+			TIM4->CCR1 = getPWM_CCR(0.50);
+			TIM4->CCR2 = getPWM_CCR(0.50);
+			TIM4->CCR3 = getPWM_CCR(0.50);
+			TIM4->CCR4 = getPWM_CCR(0.50);
+			i = 0;
 		}
 	}
 }
@@ -581,259 +561,6 @@ static void TIM3_SetupIRQ(void)
     nvicStructure.NVIC_IRQChannelSubPriority = 0;
     nvicStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&nvicStructure);
-}
-
-/* GyroConfig
- * @brief  Configures the MEMS to gyroscope application
- * @param  None
- * @retval None
- */
-static void GyroConfig(void)
-{
-	  L3GD20_InitTypeDef L3GD20_InitStructure;
-	  L3GD20_FilterConfigTypeDef L3GD20_FilterStructure;
-
-	  /* Configure Mems L3GD20 */
-	  L3GD20_InitStructure.Power_Mode = L3GD20_MODE_ACTIVE;
-	  L3GD20_InitStructure.Output_DataRate = L3GD20_OUTPUT_DATARATE_1;
-	  L3GD20_InitStructure.Axes_Enable = L3GD20_AXES_ENABLE;
-	  L3GD20_InitStructure.Band_Width = L3GD20_BANDWIDTH_4;
-	  L3GD20_InitStructure.BlockData_Update = L3GD20_BlockDataUpdate_Continous;
-	  L3GD20_InitStructure.Endianness = L3GD20_BLE_LSB;
-	  L3GD20_InitStructure.Full_Scale = L3GD20_FULLSCALE_500;
-	  L3GD20_Init(&L3GD20_InitStructure);
-
-	  L3GD20_FilterStructure.HighPassFilter_Mode_Selection = L3GD20_HPM_NORMAL_MODE_RES;
-	  L3GD20_FilterStructure.HighPassFilter_CutOff_Frequency = L3GD20_HPFCF_0;
-	  L3GD20_FilterConfig(&L3GD20_FilterStructure) ;
-
-	  L3GD20_FilterCmd(L3GD20_HIGHPASSFILTER_ENABLE);
-}
-
-/**
-  * @brief  Calculate the angular Data rate Gyroscope.
-  * @param  pfData : Data out pointer
-  * @retval None
-  */
-static void GyroReadAngRate(float* pfData)
-{
-  uint8_t tmpbuffer[6] ={0};
-  int16_t RawData[3] = {0};
-  uint8_t tmpreg = 0;
-  float sensitivity = 0;
-  int i =0;
-
-  L3GD20_Read(&tmpreg,L3GD20_CTRL_REG4_ADDR,1);
-
-  L3GD20_Read(tmpbuffer,L3GD20_OUT_X_L_ADDR,6);
-
-  /* check in the control register 4 the data alignment (Big Endian or Little Endian)*/
-  if(!(tmpreg & 0x40))
-  {
-    for(i=0; i<3; i++)
-    {
-      RawData[i]=(int16_t)(((uint16_t)tmpbuffer[2*i+1] << 8) + tmpbuffer[2*i]);
-    }
-  }
-  else
-  {
-    for(i=0; i<3; i++)
-    {
-      RawData[i]=(int16_t)(((uint16_t)tmpbuffer[2*i] << 8) + tmpbuffer[2*i+1]);
-    }
-  }
-
-  /* Switch the sensitivity value set in the CRTL4 */
-  switch(tmpreg & 0x30)
-  {
-  case 0x00:
-    sensitivity = L3G_Sensitivity_250dps;
-    break;
-
-  case 0x10:
-    sensitivity = L3G_Sensitivity_500dps;
-    break;
-
-  case 0x20:
-    sensitivity = L3G_Sensitivity_2000dps;
-    break;
-  }
-  /* divide by sensitivity */
-  for(i=0; i<3; i++)
-  {
-    pfData[i] = (float)RawData[i]/sensitivity;
-  }
-}
-
-/**
-  * @brief  Configure the Mems to compass application.
-  * @param  None
-  * @retval None
-  */
-static void CompassConfig(void)
-{
-  LSM303DLHCMag_InitTypeDef LSM303DLHC_InitStructure;
-  LSM303DLHCAcc_InitTypeDef LSM303DLHCAcc_InitStructure;
-  LSM303DLHCAcc_FilterConfigTypeDef LSM303DLHCFilter_InitStructure;
-
-  /* Configure MEMS magnetometer main parameters: temp, working mode, full Scale and Data rate */
-  LSM303DLHC_InitStructure.Temperature_Sensor = LSM303DLHC_TEMPSENSOR_DISABLE;
-  LSM303DLHC_InitStructure.MagOutput_DataRate =LSM303DLHC_ODR_30_HZ ;
-  LSM303DLHC_InitStructure.MagFull_Scale = LSM303DLHC_FS_8_1_GA;
-  LSM303DLHC_InitStructure.Working_Mode = LSM303DLHC_CONTINUOS_CONVERSION;
-  LSM303DLHC_MagInit(&LSM303DLHC_InitStructure);
-
-   /* Fill the accelerometer structure */
-  LSM303DLHCAcc_InitStructure.Power_Mode = LSM303DLHC_NORMAL_MODE;
-  LSM303DLHCAcc_InitStructure.AccOutput_DataRate = LSM303DLHC_ODR_50_HZ;
-  LSM303DLHCAcc_InitStructure.Axes_Enable= LSM303DLHC_AXES_ENABLE;
-  LSM303DLHCAcc_InitStructure.AccFull_Scale = LSM303DLHC_FULLSCALE_2G;
-  LSM303DLHCAcc_InitStructure.BlockData_Update = LSM303DLHC_BlockUpdate_Continous;
-  LSM303DLHCAcc_InitStructure.Endianness=LSM303DLHC_BLE_LSB;
-  LSM303DLHCAcc_InitStructure.High_Resolution=LSM303DLHC_HR_ENABLE;
-  /* Configure the accelerometer main parameters */
-  LSM303DLHC_AccInit(&LSM303DLHCAcc_InitStructure);
-
-  /* Fill the accelerometer LPF structure */
-  LSM303DLHCFilter_InitStructure.HighPassFilter_Mode_Selection =LSM303DLHC_HPM_NORMAL_MODE;
-  LSM303DLHCFilter_InitStructure.HighPassFilter_CutOff_Frequency = LSM303DLHC_HPFCF_16;
-  LSM303DLHCFilter_InitStructure.HighPassFilter_AOI1 = LSM303DLHC_HPF_AOI1_DISABLE;
-  LSM303DLHCFilter_InitStructure.HighPassFilter_AOI2 = LSM303DLHC_HPF_AOI2_DISABLE;
-
-  /* Configure the accelerometer LPF main parameters */
-  LSM303DLHC_AccFilterConfig(&LSM303DLHCFilter_InitStructure);
-}
-
-/**
-* @brief Read LSM303DLHC output register, and calculate the acceleration ACC=(1/SENSITIVITY)* (out_h*256+out_l)/16 (12 bit rappresentation)
-* @param pnData: pointer to float buffer where to store data
-* @retval None
-*/
-static void CompassReadAcc(float* pfData)
-{
-  int16_t pnRawData[3];
-  uint8_t ctrlx[2];
-  uint8_t buffer[6], cDivider;
-  uint8_t i = 0;
-  float LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_2g;
-
-  /* Read the register content */
-  LSM303DLHC_Read(ACC_I2C_ADDRESS, LSM303DLHC_CTRL_REG4_A, ctrlx,2);
-  LSM303DLHC_Read(ACC_I2C_ADDRESS, LSM303DLHC_OUT_X_L_A, buffer, 6);
-
-  if(ctrlx[1]&0x40)
-    cDivider=64;
-  else
-    cDivider=16;
-
-  /* check in the control register4 the data alignment*/
-  if(!(ctrlx[0] & 0x40) || (ctrlx[1] & 0x40)) /* Little Endian Mode or FIFO mode */
-  {
-    for(i=0; i<3; i++)
-    {
-      pnRawData[i]=((int16_t)((uint16_t)buffer[2*i+1] << 8) + buffer[2*i])/cDivider;
-    }
-  }
-  else /* Big Endian Mode */
-  {
-    for(i=0; i<3; i++)
-      pnRawData[i]=((int16_t)((uint16_t)buffer[2*i] << 8) + buffer[2*i+1])/cDivider;
-  }
-  /* Read the register content */
-  LSM303DLHC_Read(ACC_I2C_ADDRESS, LSM303DLHC_CTRL_REG4_A, ctrlx,2);
-
-
-  if(ctrlx[1]&0x40)
-  {
-    /* FIFO mode */
-    LSM_Acc_Sensitivity = 0.25;
-  }
-  else
-  {
-    /* normal mode */
-    /* switch the sensitivity value set in the CRTL4*/
-    switch(ctrlx[0] & 0x30)
-    {
-    case LSM303DLHC_FULLSCALE_2G:
-      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_2g;
-      break;
-    case LSM303DLHC_FULLSCALE_4G:
-      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_4g;
-      break;
-    case LSM303DLHC_FULLSCALE_8G:
-      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_8g;
-      break;
-    case LSM303DLHC_FULLSCALE_16G:
-      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_16g;
-      break;
-    }
-  }
-
-  /* Obtain the mg value for the three axis */
-  for(i=0; i<3; i++)
-  {
-    pfData[i]=(float)pnRawData[i]/LSM_Acc_Sensitivity;
-  }
-
-}
-
-/**
-  * @brief  calculate the magnetic field Magn.
-* @param  pfData: pointer to the data out
-  * @retval None
-  */
-static void CompassReadMag(float* pfData)
-{
-  static uint8_t buffer[6] = {0};
-  uint8_t CTRLB = 0;
-  uint16_t Magn_Sensitivity_XY = 0, Magn_Sensitivity_Z = 0;
-  uint8_t i =0;
-  LSM303DLHC_Read(MAG_I2C_ADDRESS, LSM303DLHC_CRB_REG_M, &CTRLB, 1);
-
-  LSM303DLHC_Read(MAG_I2C_ADDRESS, LSM303DLHC_OUT_X_H_M, buffer, 1);
-  LSM303DLHC_Read(MAG_I2C_ADDRESS, LSM303DLHC_OUT_X_L_M, buffer+1, 1);
-  LSM303DLHC_Read(MAG_I2C_ADDRESS, LSM303DLHC_OUT_Y_H_M, buffer+2, 1);
-  LSM303DLHC_Read(MAG_I2C_ADDRESS, LSM303DLHC_OUT_Y_L_M, buffer+3, 1);
-  LSM303DLHC_Read(MAG_I2C_ADDRESS, LSM303DLHC_OUT_Z_H_M, buffer+4, 1);
-  LSM303DLHC_Read(MAG_I2C_ADDRESS, LSM303DLHC_OUT_Z_L_M, buffer+5, 1);
-  /* Switch the sensitivity set in the CRTLB*/
-  switch(CTRLB & 0xE0)
-  {
-  case LSM303DLHC_FS_1_3_GA:
-    Magn_Sensitivity_XY = LSM303DLHC_M_SENSITIVITY_XY_1_3Ga;
-    Magn_Sensitivity_Z = LSM303DLHC_M_SENSITIVITY_Z_1_3Ga;
-    break;
-  case LSM303DLHC_FS_1_9_GA:
-    Magn_Sensitivity_XY = LSM303DLHC_M_SENSITIVITY_XY_1_9Ga;
-    Magn_Sensitivity_Z = LSM303DLHC_M_SENSITIVITY_Z_1_9Ga;
-    break;
-  case LSM303DLHC_FS_2_5_GA:
-    Magn_Sensitivity_XY = LSM303DLHC_M_SENSITIVITY_XY_2_5Ga;
-    Magn_Sensitivity_Z = LSM303DLHC_M_SENSITIVITY_Z_2_5Ga;
-    break;
-  case LSM303DLHC_FS_4_0_GA:
-    Magn_Sensitivity_XY = LSM303DLHC_M_SENSITIVITY_XY_4Ga;
-    Magn_Sensitivity_Z = LSM303DLHC_M_SENSITIVITY_Z_4Ga;
-    break;
-  case LSM303DLHC_FS_4_7_GA:
-    Magn_Sensitivity_XY = LSM303DLHC_M_SENSITIVITY_XY_4_7Ga;
-    Magn_Sensitivity_Z = LSM303DLHC_M_SENSITIVITY_Z_4_7Ga;
-    break;
-  case LSM303DLHC_FS_5_6_GA:
-    Magn_Sensitivity_XY = LSM303DLHC_M_SENSITIVITY_XY_5_6Ga;
-    Magn_Sensitivity_Z = LSM303DLHC_M_SENSITIVITY_Z_5_6Ga;
-    break;
-  case LSM303DLHC_FS_8_1_GA:
-    Magn_Sensitivity_XY = LSM303DLHC_M_SENSITIVITY_XY_8_1Ga;
-    Magn_Sensitivity_Z = LSM303DLHC_M_SENSITIVITY_Z_8_1Ga;
-    break;
-  }
-
-  for(i=0; i<2; i++)
-  {
-    pfData[i]=(float)((int16_t)(((uint16_t)buffer[2*i] << 8) + buffer[2*i+1])*1000)/Magn_Sensitivity_XY;
-  }
-  pfData[2]=(float)((int16_t)(((uint16_t)buffer[4] << 8) + buffer[5])*1000)/Magn_Sensitivity_Z;
 }
 
 
