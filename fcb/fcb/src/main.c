fc85d49f2572f39fa5f5945dc0fb9a185bcb9436
Daniel <daniel_s1987@hotmail.com>
2015-05-07 21:46:55 +0200

    Fixed main.c problems

--- fcb/fcb/src/main.c
+++ fcb/fcb/src/main.c
@@ -10,7 +10,6 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "usbd_cdc_if.h"
 #include "motor_control.h"
 #include "flight_control.h"
 #include "sensors.h"
@@ -22,12 +21,14 @@
 
 /* Private variables ---------------------------------------------------------*/
 USBD_HandleTypeDef hUSBDDevice;
-Button_TypeDef UserButton;
+volatile uint8_t UserButtonPressed;
 
 /* Private function prototypes -----------------------------------------------*/
 static void SystemClock_Config(void);
 static void Init_System(void);
 static void Init_LEDs(void);
+static void LEDs_Off(void);
+static void ToggleLEDs();
 
 /**
  * @brief  Main program.
@@ -43,6 +44,26 @@ int main(void)
    * system_stm32f30x.c file
    */
 
+  Init_System();
+
+  while (1)
+    {
+      ToggleLEDs();
+    }
+}
+
+static void Init_System(void)
+{
+  /* STM32F3xx HAL library initialization:
+         - Configure the Flash prefetch
+         - Systick timer is configured by default as source of time base, but user
+           can eventually implement his proper time base source (a general purpose
+           timer for example or other time source), keeping in mind that Time base
+           duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and
+           handled in milliseconds basis.
+         - Set NVIC Group Priority to 4
+         - Low Level Initialization
+   */
   HAL_Init();
 
   /* Configure the system clock to 72 Mhz */
@@ -60,31 +81,11 @@ int main(void)
   /* Start Device Process */
   USBD_Start(&hUSBDDevice);
 
-  Init_System();
-
-  // Infinite loop keeps the program alive.
-  uint32_t cntr = 0;
-
-  while (1)
-    {
-      HAL_Delay(2000);
-
-      BSP_LED_Off((cntr+7) % 8);
-      BSP_LED_On(cntr % 8);
-      cntr++;
-
-//      char String[32] = "Hello, this is Dragonfly\n";
-//      CDC_Transmit_FS((uint8_t*) String, sizeof(String));
-    }
-}
-
-static void Init_System(void)
-{
   /* Init on-board LEDs */
   Init_LEDs();
 
   /* Init User button */
-  BSP_PB_Init(UserButton, BUTTON_MODE_EXTI);
+  BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
 
   /* Setup sensors */
   GyroConfig();
@@ -126,6 +127,16 @@ static void Init_LEDs(void)
   BSP_LED_Init(LED9);
   BSP_LED_Init(LED10);
 
+  LEDs_Off();
+}
+
+/**
+  * @brief  Turns off all the LEDs
+  * @param  None
+  * @retval None
+  */
+static void LEDs_Off(void)
+{
   BSP_LED_Off(LED3);
   BSP_LED_Off(LED4);
   BSP_LED_Off(LED5);
@@ -134,6 +145,77 @@ static void Init_LEDs(void)
   BSP_LED_Off(LED8);
   BSP_LED_Off(LED9);
   BSP_LED_Off(LED10);
+}
+
+/**
+  * @brief Toggles the LEDs based on User Button presses
+  * @param None
+  * @retval None
+  */
+void ToggleLEDs(void)
+{
+  switch(UserButtonPressed)
+  {
+  case 0:
+    LEDs_Off();
+    BSP_LED_On(LED3);
+    break;
+
+  case 1:
+    LEDs_Off();
+    BSP_LED_On(LED4);
+    break;
+
+  case 2:
+    LEDs_Off();
+    BSP_LED_On(LED5);
+    break;
+
+  case 3:
+    LEDs_Off();
+    BSP_LED_On(LED6);
+    break;
+
+  case 4:
+    LEDs_Off();
+    BSP_LED_On(LED7);
+    break;
+
+  case 5:
+    LEDs_Off();
+    BSP_LED_On(LED8);
+    break;
+
+  case 6:
+    LEDs_Off();
+    BSP_LED_On(LED9);
+    break;
+
+  case 7:
+    LEDs_Off();
+    BSP_LED_On(LED10);
+    break;
+
+  default:
+    break;
+  }
+}
+
+/**
+  * @brief EXTI line detection callbacks
+  * @param GPIO_Pin: Specifies the pins connected EXTI line
+  * @retval None
+  */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  if(GPIO_Pin == USER_BUTTON_PIN)
+  {
+   UserButtonPressed++;
+   if (UserButtonPressed > 0x7)
+    {
+      UserButtonPressed = 0x0;
+    }
+  }
 }
 
 /**
