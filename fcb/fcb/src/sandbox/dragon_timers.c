f7833dcb30e1917c4959b9970b19969d6e44ed6e
Adam S <adam.at.epsilon@gmail.com>
2015-05-20 15:18:59 +0200

    test toggling pin PD9 for measuring FreeRTOS accuracy.
    success - a 1 ms timer comes out very accurately on pin.

--- fcb/fcb/src/sandbox/dragon_timers.c
+++ fcb/fcb/src/sandbox/dragon_timers.c
@@ -1,20 +1,34 @@
 #include "stm32f3xx.h"
 #include "stm32f3_discovery.h"
+#include "stm32f3xx_hal_gpio.h"
 
 #include "FreeRTOS.h"
 #include "timers.h"
 #include "semphr.h"
 
-/* tmrTIMER_CALLBACK */
+static GPIO_InitTypeDef  GPIO_InitStruct;
 
-static void dragon_timer_action(xTimerHandle xTimer );
+
+static void dragon_timer_action(xTimerHandle xTimer ); /* tmrTIMER_CALLBACK */
 static xTimerHandle xDragonTimer = NULL;
-enum { TIMER_INTERVAL_MS = 500 };
+enum { TIMER_INTERVAL_MS = 1 };
 enum { DRAGON_TIMER_ID = 242 };
 
 void dragon_timers(void) {
 	xSemaphoreHandle sNeverExit = xSemaphoreCreateBinary();
 	BSP_LED_On(LED3);
+
+	/* configure pin PD9 for GPIO toggling */
+
+	 __GPIOD_CLK_ENABLE();
+
+	  /* -2- Configure PE.8 to PE.15 IOs in output push-pull mode to drive external LEDs */
+	 GPIO_InitStruct.Pin = (GPIO_PIN_9);
+	 GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	 GPIO_InitStruct.Pull = GPIO_PULLUP;
+	 GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+
+	  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
 	if (0 == (xDragonTimer = xTimerCreate((const signed char*)"tmrDragon",
 			TIMER_INTERVAL_MS / portTICK_RATE_MS,
@@ -47,6 +61,8 @@ void dragon_timers(void) {
 
 static void dragon_timer_action(xTimerHandle xTimer ) {
 	static uint8_t flipflop = 0;
+
+    HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_9);
 
 	if (flipflop) {
 		BSP_LED_Off(LED6);
