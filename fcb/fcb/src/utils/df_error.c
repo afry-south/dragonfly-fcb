0abf255480d5a3e505eed1bedda4eece49f1fd13
Adam S <adam.at.epsilon@gmail.com>
2015-06-03 16:22:54 +0200

    added utils/df_error.h error handling utility

---
+++ fcb/fcb/src/utils/df_error.c
@@ -0,0 +1,11 @@
+#include "df_error.h"
+
+void df_error(void) {
+    BSP_LED_On(LED10);
+    BSP_LED_On(LED3);
+    /* TODO in the future this function should accept a text
+     * string that could be printed to USB, or elsewhere
+     */
+    while(1) {
+    }
+}
