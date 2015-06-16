4bc850ecf0a03b7a89720d277d589a5f49647a2f
Adam S <adam.at.epsilon@gmail.com>
2015-06-16 15:24:58 +0200

    df_error now switches on all LEDs

--- fcb/fcb/src/utils/df_error.c
+++ fcb/fcb/src/utils/df_error.c
@@ -4,8 +4,16 @@
 
 
 void df_error(void) {
-    BSP_LED_On(LED10);
     BSP_LED_On(LED3);
+    BSP_LED_On(LED4);
+    BSP_LED_On(LED5);
+    BSP_LED_On(LED6);
+    BSP_LED_On(LED7);
+    BSP_LED_On(LED8);
+    BSP_LED_On(LED9);
+    BSP_LED_On(LED10);
+
+
     /* TODO in the future this function should accept a text
      * string that could be printed to USB, or elsewhere
      */
