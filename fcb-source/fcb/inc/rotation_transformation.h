69df72b3408ba0178c017fc2f4132995fbe12774
Daniel Stenberg <daniel_s1987@hotmail.com>
2015-09-24 13:39:02 +0200

    Issue #129. Large changes to file path tree to simplify paths and thereby fix too large command line string issue when make is run by Eclipse.

---
+++ fcb-source/fcb/inc/rotation_transformation.h
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * @file    rotation_transformation.h
+ * @brief   Header file for coordinate system rotation transformation module
+ ******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef INC_ROTATION_TRANSFORMATION_H_
+#define INC_ROTATION_TRANSFORMATION_H_
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+#endif /* INC_ROTATION_TRANSFORMATION_H_ */
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+/*****END OF FILE****/
---
+++ fcb-source/fcb/src/rotation_transformation.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * @file    rotation_transformation.c
+ * @brief   Functions to keep track of coordinate system representations and
+ * 			transformations between world and body frames. The transformations
+ * 			are based on Euler angle rotations (Z-Y-X / roll-pitch-yaw) and
+ * 			rotation matrices.
+ ******************************************************************************/
+
+/* Includes ------------------------------------------------------------------*/
+#include "rotation_transformation.h"
+
+#include <math.h>
+#include "arm_math.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+/*****END OF FILE****/
