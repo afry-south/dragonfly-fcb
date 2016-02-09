a9c29f927109b28be121b090907943ac3136743d
Daniel Stenberg <daniel_s1987@hotmail.com>
2016-02-09 20:16:14 +0100

    IO pin init for UART using DMA.

---
+++ fcb-source/communication/uart/inc/uart.h
@@ -0,0 +1,56 @@
+/*****************************************************************************
+ * @file    motor_control.h
+ * @brief   Header file for UART handling module
+ ******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __UART_H
+#define __UART_H
+
+/* Includes ------------------------------------------------------------------*/
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Definition for USARTx clock resources */
+#define UART                           	USART1
+#define UART_CLK_ENABLE()             	__USART2_CLK_ENABLE();
+#define DMA_CLK_ENABLE()				__DMA1_CLK_ENABLE()
+#define UART_RX_GPIO_CLK_ENABLE()		__GPIOA_CLK_ENABLE()
+#define UART_TX_GPIO_CLK_ENABLE()		__GPIOA_CLK_ENABLE()
+
+/* Definition for UART Pins */
+#define UART_TX_PIN                    	GPIO_PIN_2
+#define UART_TX_GPIO_PORT              	GPIOA
+#define UART_TX_AF                     	GPIO_AF7_USART2
+#define UART_RX_PIN                    	GPIO_PIN_3
+#define UART_RX_GPIO_PORT              	GPIOA
+#define UART_RX_AF                     	GPIO_AF7_USART2
+
+/* Definition for UART's DMA */
+#define UART_TX_DMA_STREAM            	DMA1_Channel4
+#define UART_RX_DMA_STREAM              DMA1_Channel5
+
+/* Definition for UART's NVIC */
+#define UART_DMA_TX_IRQn               	DMA1_Channel4_IRQn
+#define UART_DMA_RX_IRQn                DMA1_Channel5_IRQn
+#define UART_DMA_TX_IRQHandler          DMA1_Channel4_IRQHandler
+#define UART_DMA_RX_IRQHandler          DMA1_Channel5_IRQHandler
+
+/* Exported macro ------------------------------------------------------------*/
+
+
+/* Exported functions ------------------------------------------------------- */
+
+
+#endif /* __UART_H */
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+/*****END OF FILE****/
