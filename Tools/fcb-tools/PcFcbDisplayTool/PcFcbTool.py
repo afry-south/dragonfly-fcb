f45fa72fd27c363741fe27ab2617804d1c2aec0e
Adam Steineck <adam.at.epsilon@gmail.com>
2016-02-26 19:09:45 +0100

    Issue 138: better error handling for the serial COM port

--- Tools/fcb-tools/PcFcbDisplayTool/PcFcbTool.py
+++ Tools/fcb-tools/PcFcbDisplayTool/PcFcbTool.py
@@ -7,57 +7,53 @@ import serial
 import threading
 from serial.tools.list_ports import comports
 import dragonfly_fcb_pb2
-import PcFcbDisplayPlot
 import google.protobuf.message
-from __builtin__ import str, int
-
+from collections import deque
 
 print "imports successful, Captain!"
 
 print "OS name:" + os.name
 
 mySerial = 0;
+doExit = False;
+sema = threading.Condition()
 
-# NOTE:
-# this function is only semi-useful because it doesn't print
-# the correct format of the device address to respective port
-def ask_for_port():
-    """\
-    Show a list of ports and ask the user for a choice. To make selection
-    easier on systems with long device names, also allow the input of an
-    index.
-    """
-    sys.stderr.write('\n--- Available ports:\n')
-    ports = []
-    for n, (port, desc, hwid) in enumerate(sorted(comports()), 1):
-        #~ sys.stderr.write('--- %-20s %s [%s]\n' % (port, desc, hwid))
-        sys.stderr.write('--- {:2}: {:20} {}\n'.format(n, port, desc))
-        ports.append(port)
-    while True:
-        port = raw_input('--- Enter port index or full name: ')
-        try:
-            index = int(port) - 1
-            if not 0 <= index < len(ports):
-                sys.stderr.write('--- Invalid index!\n')
-                continue
-        except ValueError:
-            pass
-        else:
-            port = ports[index]
-        return port
+roll_data = deque([float(0)]*100) # append & pop from opposite ends is thread safe according to .py doc 
+pitch_data = deque([float(0)]*100) # append & pop from opposite ends is thread safe according to .py doc 
+yaw_data = deque([float(0)]*100) # append & pop from opposite ends is thread safe according to .py doc 
 
-# ask_for_port()
+def update_plot_data(new_roll_data, new_pitch_data, new_yaw_data):
+    sema.acquire(blocking = 1)
+    roll_data.appendleft(new_roll_data)
+    datatoplot = roll_data.pop() # only want 100 values to plot at any one time
+    pitch_data.appendleft(new_roll_data)
+    datatoplot = pitch_data.pop() # only want 100 values to plot at any one time    
+    roll_data.appendleft(new_yaw_data)
+    datatoplot = yaw_data.pop() # only want 100 values to plot at any one time    
+    
+    sema.notify() # notify plot that data is available
+    sema.release()
 
-def comReader():
+# handy tip, type: python -m serial.tools.list_ports
+# to list available COM ports at the terminal.
+
+def comReader(sample_nbr):
+    global doExit
     print "comReader ready, Captain!!"
     print "comReader is executing in: " + threading.currentThread().name
     state = dragonfly_fcb_pb2.FlightStatesProto()
+    i = 0;
 
-    while 1:
+    while (i < sample_nbr and not doExit):
         line = ""
         data = ' '
         while (data != '\r'):
-            data = mySerial.read(1);
+            try:
+                data = mySerial.read(1);
+            except serial.SerialException as se:
+                print se
+                doExit = True
+                break
             if (data != '\n') and (data != '\r'):
                 line += str(data)
 
@@ -68,26 +64,48 @@ def comReader():
             except google.protobuf.message.DecodeError as de:
                 print de
                 continue
-            # sys.stdout.write(state.__str__());
-            PcFcbDisplayPlot.add_plot_data(state.rollAngle, state.pitchAngle, state.yawAngle)
+            sys.stdout.write(state.__str__());
+            update_plot_data(state.rollAngle, state.pitchAngle, state.yawAngle)
+            i += 1;
+            print "sample i:%d" % (i)
         else:
+            print line
             pass
+                
             # print line
-
-        
-    print "exiting while loop"
-
-interval_ms = 500
-duration_s = 20
+    mySerial.close()
+    print "exiting comReader loop"
 
 # use "import optparse" in future - see miniterm.py in Python installation for example.
 # this will allow sophisticaed CLI args parsing
-mySerial = serial.serial_for_url("\\.\COM8", 115200, parity='N', rtscts=False, xonxoff=False, timeout=1)
-myComReaderThread = threading.Thread(target=comReader, name="tComReader")
+
+interval_ms = 500
+duration_s = 5
+
+interval_s = interval_ms/float(1000)
+sample_nbr = int(float(duration_s / interval_s))
+
+# use "import optparse" in future - see miniterm.py in Python installation for example.
+# this will allow sophisticaed CLI args parsing
+mySerial = serial.Serial("\\.\COM8", 115200, parity=serial.PARITY_NONE, rtscts=False, xonxoff=False, timeout=1, write_timeout=1)
+myComReaderThread = threading.Thread(target=comReader, name="tComReader", args=(sample_nbr,))
 myComReaderThread.daemon = True
 myComReaderThread.start()
-mySerial.write("about\n".encode()) # todo
-PcFcbDisplayPlot.runPlot()
+mySerial.write("about\n") # data does not show up when removing this line ...
 time.sleep(1)
-mySerial.write("start-state-sampling %d %d p" % (interval_ms, duration_s))
+try:
+    mySerial.write("start-state-sampling %d %d p" % (interval_ms, duration_s))
+except serial.SerialTimeoutException as ste:
+    mySerial.close()
+    print ste
+    quit()
+    
 myComReaderThread.join(duration_s + 1)
+if myComReaderThread.isAlive():
+    doExit = True
+    mySerial.close()
+    time.sleep(interval_s)
+    
+print "after join"
+if mySerial.is_open:
+    mySerial.close()
