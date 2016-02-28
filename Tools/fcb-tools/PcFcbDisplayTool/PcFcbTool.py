d5e3b6785fb7a662289f57a0cdc7eb6a078fcd79
Adam Steineck <adam.at.epsilon@gmail.com>
2016-02-28 21:45:27 +0100

    Issue #138: graphics based on pyqtgraph
    - installation instruction for pyqtgraph in README.md
    - roll, pitch & yaw  data shows in real-time plot
    - error messages are printed to console
    - investigate number of samples actually received - a discrepancy?
    - added To Do in README.md
     - interval & duration of sampling is hard coded - use argparse to fix

--- Tools/fcb-tools/PcFcbDisplayTool/PcFcbTool.py
+++ Tools/fcb-tools/PcFcbDisplayTool/PcFcbTool.py
@@ -1,8 +1,8 @@
 # borrowed ideas from miniterm.py, part of Python2.7 distribution.
 
 import sys
-import time
 import os
+import time
 import serial
 import threading
 from serial.tools.list_ports import comports
@@ -10,12 +10,28 @@ import dragonfly_fcb_pb2
 import google.protobuf.message
 from collections import deque
 
-print "imports successful, Captain!"
+from pyqtgraph.Qt import QtGui, QtCore
+import numpy as np
+import pyqtgraph as pg
 
-print "OS name:" + os.name
+debug_print = False
+script_name = os.path.basename(__file__)[:-3]
 
-mySerial = 0;
-doExit = False;
+def pprint(format, *args):
+    string_to_print = script_name + ": " + format % args
+    print string_to_print
+
+def dprint(format, *args):
+    string_to_print = script_name + ": " + format % args
+    if debug_print:
+        pprint(string_to_print)
+        
+pprint("starting ...")
+
+dprint("imports successful, Captain!")
+
+fcb_serial = 0;
+do_exit = False;
 sema = threading.Condition()
 
 roll_data = deque([float(0)]*100) # append & pop from opposite ends is thread safe according to .py doc 
@@ -26,9 +42,9 @@ def update_plot_data(new_roll_data, new_pitch_data, new_yaw_data):
     sema.acquire(blocking = 1)
     roll_data.appendleft(new_roll_data)
     datatoplot = roll_data.pop() # only want 100 values to plot at any one time
-    pitch_data.appendleft(new_roll_data)
+    pitch_data.appendleft(new_pitch_data)
     datatoplot = pitch_data.pop() # only want 100 values to plot at any one time    
-    roll_data.appendleft(new_yaw_data)
+    yaw_data.appendleft(new_yaw_data)
     datatoplot = yaw_data.pop() # only want 100 values to plot at any one time    
     
     sema.notify() # notify plot that data is available
@@ -38,74 +54,115 @@ def update_plot_data(new_roll_data, new_pitch_data, new_yaw_data):
 # to list available COM ports at the terminal.
 
 def comReader(sample_nbr):
-    global doExit
-    print "comReader ready, Captain!!"
-    print "comReader is executing in: " + threading.currentThread().name
+    global do_exit
+    pprint("%s starting" % comReader.__name__)
+    dprint("comReader is executing in: " + threading.currentThread().name)
     state = dragonfly_fcb_pb2.FlightStatesProto()
     i = 0;
 
-    while (i < sample_nbr and not doExit):
+    while (i < sample_nbr and not do_exit):
         line = ""
         data = ' '
         while (data != '\r'):
             try:
-                data = mySerial.read(1);
+                data = fcb_serial.read(1);
             except serial.SerialException as se:
-                print se
-                doExit = True
+                pprint("serial.SerialException: %s" % (se.__str__()))
+                do_exit = True
                 break
             if (data != '\n') and (data != '\r'):
                 line += str(data)
-
+        
+        if do_exit:
+            break
+        
+        i += 1;
         hexified = ':'.join(x.encode('hex') for x in line)
         if hexified[:2] == "04":
             try:
                 state.ParseFromString(line[4:]);
             except google.protobuf.message.DecodeError as de:
-                print de
+                pprint("sample %d discarded: %s" % (i, de.__str__()))
                 continue
-            sys.stdout.write(state.__str__());
+            # sys.stdout.write(state.__str__()); # useful for debugging
             update_plot_data(state.rollAngle, state.pitchAngle, state.yawAngle)
-            i += 1;
-            print "sample i:%d" % (i)
         else:
-            print line
-            pass
-                
-            # print line
-    mySerial.close()
-    print "exiting comReader loop"
+            pprint("%s received: %s", comReader.__name__, line)
 
-# use "import optparse" in future - see miniterm.py in Python installation for example.
-# this will allow sophisticaed CLI args parsing
+    fcb_serial.close()
+    pprint("%s done - close graphics window to exit" % comReader.__name__)
 
-interval_ms = 500
-duration_s = 5
+
+#### Graphics objects & helper functions
+
+app = QtGui.QApplication([])
+
+win = pg.GraphicsWindow(title="Basic plotting examples")
+win.resize(1000,600)
+win.setWindowTitle('pyqtgraph example: Plotting')
+pg.setConfigOptions(antialias=True) # Enable antialiasing for prettier plots
+
+roll_plot = win.addPlot(title="Real-time ish roll angle[rad] plot")
+roll_plot_curve = roll_plot.plot(pen='r', name='Roll[rad]')
+roll_plot.showGrid(x=True, y=True)
+win.nextRow()
+pitch_plot = win.addPlot(title="Real-time ish pitch angle[rad] plot")
+pitch_plot_curve = pitch_plot.plot(pen='g', name='Pitch[rad]')
+pitch_plot.showGrid(x=True, y=True)
+win.nextRow()
+yaw_plot = win.addPlot(title="Real-time ish yaw angle[rad] plot")
+yaw_plot_curve = yaw_plot.plot(pen='y', name='Yaw[rad]')
+yaw_plot.showGrid(x=True, y=True)
+
+def plotUpdate():
+    global ptr, roll_plot, pitch_plot, yaw_plot
+    roll_plot_curve.setData(roll_data)
+    pitch_plot_curve.setData(pitch_data)
+    yaw_plot_curve.setData(yaw_data)
+
+
+timer = QtCore.QTimer()
+timer.timeout.connect(plotUpdate)
+timer.start(25)
+
+# use "import argparse" in future - this will allow sophisticaed CLI args parsing
+
+interval_ms = 100
+duration_s = 20
 
 interval_s = interval_ms/float(1000)
 sample_nbr = int(float(duration_s / interval_s))
 
 # use "import optparse" in future - see miniterm.py in Python installation for example.
 # this will allow sophisticaed CLI args parsing
-mySerial = serial.Serial("\\.\COM8", 115200, parity=serial.PARITY_NONE, rtscts=False, xonxoff=False, timeout=1, write_timeout=1)
+fcb_serial = serial.Serial("\\.\COM8", 115200, parity=serial.PARITY_NONE, rtscts=False, xonxoff=False, timeout=1, write_timeout=1)
 myComReaderThread = threading.Thread(target=comReader, name="tComReader", args=(sample_nbr,))
 myComReaderThread.daemon = True
 myComReaderThread.start()
-mySerial.write("about\n") # data does not show up when removing this line ...
+fcb_serial.write("about\n") # data does not show up when removing this line ...
 time.sleep(1)
+
 try:
-    mySerial.write("start-state-sampling %d %d p" % (interval_ms, duration_s))
+    fcb_serial.write("start-state-sampling %d %d p" % (interval_ms, duration_s))
 except serial.SerialTimeoutException as ste:
-    mySerial.close()
-    print ste
+    fcb_serial.close()
+    pprint(ste.__str__())
     quit()
-    
+
+if __name__ == '__main__':
+    ## Start Qt event loop unless running in interactive mode or using pyside.
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        pprint("close window to exit program")
+        QtGui.QApplication.instance().exec_()
+        do_exit = True
+
 myComReaderThread.join(duration_s + 1)
 if myComReaderThread.isAlive():
-    doExit = True
-    mySerial.close()
+    do_exit = True
+    fcb_serial.close()
     time.sleep(interval_s)
     
-print "after join"
-if mySerial.is_open:
-    mySerial.close()
+pprint("... please wait for program to exit")
+if fcb_serial.is_open:
+    fcb_serial.close()
--- Tools/fcb-tools/PcFcbDisplayTool/README.md
+++ Tools/fcb-tools/PcFcbDisplayTool/README.md
@@ -3,11 +3,15 @@
 ## Python installation
 * General python installation:See Dragonfly-FCB Wiki page "ToolTipsTricks", section "Python" and follow installation instructions.
 * Open `cmd.exe`
-* Install library dependencies: `C:\INSTALLATION_PATH\python.exe -m pip install pyserial numpy matplotlib protobuf
+* Install library dependencies: `C:\INSTALLATION_PATH\python.exe -m pip install pyserial numpy protobuf pyside pyqtgraph`
  * If the `error: Microsoft Visual C++ 9.0 is required (Unable to find vcvarsall.bat)` occurs, Get it from http://aka.ms/vcpython27
+* Run `C:\INSTALLATION_PATH\python -m pip install pyside pyqtgraph`
 * now it should be possible to run `C:\INSTALLATION_PATH\python.exe PcFcbTool.py` in `cmd.exe`.
 
 ## Developing PcFcbDisplayTool
 * In order to regenerate `dragonfly_fcb_pb2.py`:
  * Download `protoc-3.0.0-beta-2-win32.zip` from: https://github.com/google/protobuf/releases extract `protoc.exe` and make sure it is in the `Path` of `cmd.exe`
  * Then run: `protoc --plugin=generator\protoc-gen-nanopb.bat --proto_path=REPO_PATH\fcb-source\communication\protobuf\ REPO_PATH\fcb-source\communication\protobuf\dragonfly_fcb.proto  --python_out=REPO_PATH\Tools\fcb-tools\PcFcbDisplayTool
+
+## To Do
+ * use `argparse` for CLI options parsing
