61d83a8cdd616a0497259e3c9a148e9f37f49236
Johan Svärd <superdupersvard@gmail.com>
2016-09-16 09:46:58 +0200

    Add limits for motors so that when in roll/pitch if one motor is decreased to zero the adjacent motor is not increased any further.

--- fcb-source/fcb/src/motor_control.c
+++ fcb-source/fcb/src/motor_control.c
@@ -436,14 +436,31 @@ static void SaturateMotorSignalValues(int32_t* m1, int32_t* m2, int32_t* m3, int
 		*m4 = UINT16_MAX;
 
 	/* Check unsigned 16-bit underflow (less than zero) */
-	if (!IS_POS(*m1))
+	if (!IS_POS(*m1) && !IS_POS(*m3)) {
 		*m1 = 0;
-	if (!IS_POS(*m2))
-		*m2 = 0;
-	if (!IS_POS(*m3))
 		*m3 = 0;
-	if (!IS_POS(*m4))
+	}
+	if (!IS_POS(*m2) && !IS_POS(*m4)) {
+		*m2 = 0;
 		*m4 = 0;
+	}
+
+	if (!IS_POS(*m1)) {
+		*m3 += *m1;
+		*m1 = 0;
+	}
+	if (!IS_POS(*m2)) {
+		*m4 += *m2;
+		*m2 = 0;
+	}
+	if (!IS_POS(*m3)) {
+		*m1 += *m3;
+		*m3 = 0;
+	}
+	if (!IS_POS(*m4)) {
+		*m2 += *m4;
+		*m4 = 0;
+	}
 }
 
 /**
